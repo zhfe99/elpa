24b99ed64cd417ce2c409731914551511dbdf4c3
Feng Zhou <zhfe99@gmail.com>
2017-05-16 09:52:00 +0800

    Install dired-imenu

---
+++ dired-imenu-20140109.810/dired-imenu-autoloads.el
@@ -0,0 +1,15 @@
+;;; dired-imenu-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dired-imenu.el") (22810 23132 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dired-imenu-autoloads.el ends here
---
+++ dired-imenu-20140109.810/dired-imenu-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-imenu" "20140109.810" "imenu binding for dired mode" 'nil :commit "610e21fe0988c85931d34894d3eee2442c79ab0a" :url "https://github.com/DamienCassou/dired-imenu" :keywords '("dired" "imenu"))
