04d90edfa924949215efd9ba6e4c5674dd586740
Feng Zhou <zhfe99@gmail.com>
2023-03-31 22:59:26 +0800

    Update to new version

---
+++ consult-20230330.1104/consult-pkg.el
@@ -0,0 +1,11 @@
+(define-package "consult" "20230330.1104" "Consulting completing-read"
+  '((emacs "27.1")
+    (compat "29.1.4.1"))
+  :commit "16ec34dd12a734b231bcc0fa03a1ab40a86def93" :authors
+  '(("Daniel Mendler and Consult contributors"))
+  :maintainer
+  '("Daniel Mendler" . "mail@daniel-mendler.de")
+  :url "https://github.com/minad/consult")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
