4cb04e157b720a1dc97e8f90a6ac648d32bd2559
Feng <feng@feng-mini.local>
2024-11-29 14:03:27 -0800

    Update window-numbering

---
+++ window-numbering-20160809.1810/window-numbering-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "window-numbering" "20160809.1810"
+  "Numbered window shortcuts."
+  ()
+  :url "https://github.com/nschum/window-numbering.el"
+  :commit "10809b3993a97c7b544240bf5d7ce9b1110a1b89"
+  :revdesc "10809b3993a9"
+  :keywords '("faces" "matching")
+  :authors '(("Nikolaj Schumacher" . "bugs*nschumde"))
+  :maintainers '(("Nikolaj Schumacher" . "bugs*nschumde")))
